78d20eabc7348c4650bb1caad632740744e5b776
bhlbuu <bhlbuu@b6243d83-3e35-bad2-eac4-1d342526e9e5>
2010-09-22 18:57:29 +0000

    simple GUI

---
+++ trunk/ObjectDetection/ObjectDetection/detection.cpp
@@ -0,0 +1,46 @@
+#include "stdafx.h"
+#include "meanshift.h"
+extern Size cellSize,blockSize,wndSize;
+bool detection(string path,string fname, string ext,float scaleStep,float minB,double radius,int minCsize, bool justMeanshift)
+{
+	if(path[path.size()-1]!='\\')
+		path=path+"\\";
+		Mat imgOrg = imread(path+fname+ext);
+
+		cout<<path<<fname<<ext<<endl;
+		printf("%f %f %f %d\n",scaleStep,minB,radius,minCsize);
+		Mat img;
+		double maxSz=500*400.;
+		double minSz = wndSize.width*wndSize.height;
+		double t=(double)imgOrg.rows*imgOrg.cols;
+		float resizeScale=t>maxSz?sqrt(maxSz/t):1.;
+		resizeScale=t<minSz?sqrt(minSz/t):resizeScale;
+		resize(imgOrg,img,Size(imgOrg.cols*resizeScale,imgOrg.rows*resizeScale),resizeScale,resizeScale);
+		cout<<img.rows<<" "<<img.cols<<" "<<resizeScale<<endl;
+		imgOrg.release();
+		if(!justMeanshift){
+			Mat multiscale=multiscaleExp(path+fname+ext,scaleStep,minB);
+			imwrite("output/"+fname+"_multiscale.png",multiscale,vector<int>(CV_IMWRITE_PNG_COMPRESSION,4));
+			multiscale.release();
+		}
+		int n_mean,p_mean;
+		double*means;
+		meanshiftFromFile("output/"+fname+"_multiscale.txt",radius,minCsize,means,n_mean,p_mean);
+		
+		for (int i=0;i<n_mean*p_mean;i++)
+		{
+			cout<<means[i]<<", ";
+			if(i>0&&i%p_mean==p_mean-1)
+				cout<<endl;
+		}
+		drawRect2Img(img,"output/"+fname+"_meanshift.txt");
+		if(!imwrite("output/"+fname+"_meanshift.png",img,vector<int>(CV_IMWRITE_PNG_COMPRESSION,4)))
+		{
+			cout<<"KO GHI FILE DC\n";
+		}
+		
+//		imshow(fname+"_meanshift",img);
+		img.release();
+	
+	return true;
+};
--- trunk/ObjectDetection/ObjectDetection/multiscaleDetectRect.h
+++ trunk/ObjectDetection/ObjectDetection/multiscaleDetectRect.h
@@ -2,7 +2,7 @@
 #define _MULTISCALE_DETECT_RECT_H_
 
 void multiscale(Mat img,float step );
-void multiscaleExp(string filepath,float step,float minb );
+Mat multiscaleExp(string filepath,float step,float minb );
 Rect getRect(int x,int y, float scale);
 void drawRect2Img(Mat & img, string rectFile);
 
